e7fafa7f185a3f1f10c7216ead8999b51a76db9e
Aprilia Ramadhayanti <apinx01@gmail.com>
2016-10-16 17:42:02 +0700

    pertama commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<table border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td>Nama</td>
+		<td>:</td>
+		<td>Aprilia Ramadhayanti</td>
+	</tr>
+	<tr>
+		<td>NIM</td>
+		<td>:</td>
+		<td>155410010</td>
+	</tr>
+	<tr>
+		<td>Alamat</td>
+		<td>:</td>
+		<td>Jalan Magelang Km 4,5 Gang Lobak SIA V No 28B Sinduadi Mlati Sleman</td>
+	</tr>
+	<tr>
+		<td>Tempat/Tanggal Lahir</td>
+		<td>:</td>
+		<td>Samarinda, 02 April 1991</td>
+	</tr>
+</table>
